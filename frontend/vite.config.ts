6b22d6de9d77024d5690647bd317da9f582e97e0
icaniwill7 <kmichelle0113@gmail.com>
2026-05-10 01:29:55 +0900

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: isGitHubPages
+    ? "/vercel-version-youtube-music-embedding-for-music-lovers/"
+    : "/",
 });
